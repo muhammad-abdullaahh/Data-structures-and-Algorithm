c3cfc99c1f5329e6671ae035a6424203c7358c8f
Muhammad Abdullah <abdullahprivate2005@gmail.com>
2025-09-23 08:39:50 -0700

    Add files via upload

---
+++ Singly Linked lists/task01/task01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using task01;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList llist = new LinkedList();
+
+            llist.append(6);
+            llist.push(7);
+            llist.push(1);
+            llist.append(4);
+            llist.insertAfter(llist.head.next, 8);
+
+            Console.Write("Created Linked list is: ");
+            llist.printList();
+
+            llist.deleteNode(1);
+            Console.Write("\nLinked List after Deletion of 1: ");
+            llist.printList();
+        }
+    }
+}
