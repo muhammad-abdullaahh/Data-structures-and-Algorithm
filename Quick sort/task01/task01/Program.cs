e4abe3bfdd9c1edb6c6b8e4517f692ab89caa6fb
Muhammad Abdullah <abdullahprivate2005@gmail.com>
2025-12-23 21:52:37 +0500

    Add files via upload

---
+++ Quick sort/task01/task01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using task01;
+
+namespace MyFirstProgram
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] array = { 89, 12, 45, 67, 23, 91, 34, 56 };
+			QuickSort quickSorter = new QuickSort(array);
+
+			Console.WriteLine("Original Array:");
+			quickSorter.Display();
+
+			quickSorter.QSort();
+
+			Console.WriteLine("Sorted Array:");
+			quickSorter.Display();
+		}
+	}
+}
---
+++ Quick sort/task01/task01/QuickSort.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task01
+{
+	internal class QuickSort
+	{
+		public int[] arr;
+		public QuickSort(int[] array)
+		{
+			arr = array;
+		}
+		public void QSort()
+		{
+			Sort(0, arr.Length - 1);
+		}
+		private void Sort(int left, int right)
+		{
+			if (left < right)
+			{
+				int pivotIndex = Partition(left, right);
+				Sort(left, pivotIndex - 1);
+				Sort(pivotIndex + 1, right);
+			}
+		}
+
+		private int Partition(int left, int right)
+		{
+			int pivot = arr[left];
+			int i = left + 1;
+			int j = right;
+			while (true)
+			{
+				while (i <= j && arr[i] <= pivot) i++;
+				while (arr[j] > pivot) j--;
+				if (i < j)
+				{
+					Swap(i, j);
+				}
+				else
+				{
+					break;
+				}
+			}
+			Swap(left, j);
+			return j;
+		}
+
+		private void Swap(int i, int j)
+		{
+			int temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+		}
+
+		public void Display()
+		{
+			Console.WriteLine(string.Join(" ", arr));
+		}
+	}
+}
